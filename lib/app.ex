32b434d722add3c84a83a01eccaf8fbb028d3380
ILYA Khlopotov <ilya.khlopotov@gmail.com>
2014-07-11 17:48:22 -0700

    Upgrade to elixir 0.14.3-dev

--- lib/app.ex
+++ lib/app.ex
@@ -1,20 +1,22 @@
-defrecord Relex.App, name: nil, version: nil, path: nil, app: nil, type: :permanent, code_path: [] do
+defmodule Relex.App do
+  defstruct name: nil, version: nil, path: nil, app: nil, type: :permanent, code_path: []
 
-  defexception NotFound, app: nil do
-    def message(exc), do: "Application #{inspect app(exc)} not found"
+  defmodule NotFound do
+    defexception app: nil, message: ""
+    def exception(e) do
+      "Application #{inspect e.app} not found"
+    end
   end
-
-  defoverridable new: 1, app: 1, path: 1, version: 1
 
   def new(atom) when is_atom(atom), do: new(name: atom)
   def new({name, options}) when is_atom(name) do
     new(Keyword.merge([name: name], options))
   end
-  def new(opts), do: super(opts)
+  def new(opts), do: struct(%__MODULE__{}, opts)
 
   def app(rec) do
     case rec do
-      Relex.App[name: name, version: version, app: nil] ->
+      %__MODULE__{name: name, version: version, app: nil} ->
         key = {:app, {name, version}}
         case :ets.lookup(__MODULE__, key) do
           [{_, app}] -> app
@@ -23,34 +25,34 @@ defrecord Relex.App, name: nil, version: nil, path: nil, app: nil, type: :perman
             :ets.insert(__MODULE__, {key, app})
             app
         end
-      Relex.App[app: app] ->
+      %__MODULE__{app: app} ->
         app
     end
   end
 
   def path(rec) do
     case rec do
-      Relex.App[version: version, name: name, code_path: code_path, path: nil] ->
+      %__MODULE__{version: version, name: name, code_path: code_path, path: nil} ->
         case :ets.lookup(__MODULE__, {:path, {name, version}}) do
           [{_, path}] ->
             path
           _ ->
             paths = code_path
             paths = Enum.filter(paths, fn(p) -> File.exists?(Path.join([p, "#{name}.app"])) end)
-            paths = lc path inlist paths, do: Path.join(path, "..")
+            paths = for path <- paths, do: Path.join(path, "..")
             result =
             case paths do
              [] -> raise NotFound, app: rec
              [path] ->
-               if version_matches?(version, path(path, rec)) do
+               if version_matches?(version, %__MODULE__{rec | path: path}) do
                  path
                else
                  raise NotFound, app: rec
                end
              _ ->
                apps =
-               lc path inlist paths do
-                 update([path: path], rec)
+               for path <- paths do
+                 %__MODULE__{rec | path: path}
                end
                apps = Enum.filter(apps, fn(app) -> version_matches?(version, app) end)
                apps = Enum.sort(apps, fn(app1, app2) -> version(app2) <= version(app1) end)
@@ -59,7 +61,7 @@ defrecord Relex.App, name: nil, version: nil, path: nil, app: nil, type: :perman
             :ets.insert(__MODULE__, {{:path, {name, version}}, result})
             result
         end
-      Relex.App[path: path] ->
+      %__MODULE__{path: path} ->
         path
     end
   end
@@ -70,23 +72,23 @@ defrecord Relex.App, name: nil, version: nil, path: nil, app: nil, type: :perman
 
   defp version_matches?(nil, _app), do: true
   defp version_matches?(version, app) do
-    cond do
-    is_record(version, Regex) ->
-      Regex.match?(version, version(app))
-    is_function(version, 1) ->
-      version.(app)
-    true ->
-      to_string(version(app)) == to_string(version)
+    case version do
+      %Regex{} -> Regex.match?(version, version(app))
+      version when is_function(version) -> version.(app)
+      _ -> to_string(version(app)) == to_string(version)
     end
   end
 
   def dependencies(rec) do
-    (lc app inlist (keys(rec)[:applications] || []), do: code_path(code_path(rec), new(app))) ++
-    included_applications(rec)
+    for app <- (keys(rec)[:applications] || []) do
+      %__MODULE__{new(app) | code_path: rec.code_path}
+    end ++ included_applications(rec)
   end
 
   def included_applications(rec) do
-    lc app inlist (keys(rec)[:included_applications] || []), do: code_path(code_path(rec), new(app))
+    for app <- (keys(rec)[:included_applications] || []) do
+      %__MODULE__{new(app) | code_path: rec.code_path}
+    end
   end
 
   defp keys(rec) do
@@ -97,14 +99,11 @@ defrecord Relex.App, name: nil, version: nil, path: nil, app: nil, type: :perman
 end
 
 defimpl Inspect, for: Relex.App do
-  def inspect(Relex.App[name: name, version: version], _opts) do
-    cond do
-      is_record(version, Regex) ->
-       version = inspect(version)
-      is_function(version) ->
-       version = "<version checked by #{inspect(version)}>"
-      true ->
-       :ok
+  def inspect(%Relex.App{name: name, version: version}, _opts) do
+    version = case version do
+      %Regex{} -> inspect(version)
+      version when is_function(version) -> "<version checked by #{inspect(version)}>"
+      _ -> nil
     end
     version = if nil?(version), do: "", else: "-#{version}"
     "#{name}#{version}"
